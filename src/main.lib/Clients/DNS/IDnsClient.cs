05565d39c5f0215c5ed0c797e7a38b4ca4920b3d
DOH Webmasters <webmasters@flhealth.gov>
2021-07-11 14:47:31 -0400

    create interface for a DNS client that writes to delegation zone

---
+++ src/main.lib/Clients/DNS/IDnsClient.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PKISharp.WACS.Clients.DNS
+{
+    public interface IDnsClient
+    {
+        bool AddTxtRecord(string zone, string hostName, string descriptiveText);
+    }
+}
