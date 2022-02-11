27954d99749d6fc3a5c1478e197cfdcd4d6681dc
DOH Webmasters <webmasters@flhealth.gov>
2022-02-11 16:52:44 -0500

    simplify plugin name for display

--- src/main.lib/Plugins/InstallationPlugins/FileWatcher/FileWatcherOptions.cs
+++ src/main.lib/Plugins/InstallationPlugins/FileWatcher/FileWatcherOptions.cs
@@ -6,7 +6,7 @@ namespace PKISharp.WACS.Plugins.InstallationPlugins
     [Plugin("fd417f8b-a78e-47f5-85ea-b25af0e94a44")]
     public class FileWatcherOptions : InstallationPluginOptions<FileWatcher>
     {
-        public override string Name => "File Watcher";
+        public override string Name => "Watcher";
         public override string Description => "Update a certificate via a file watcher on the host machine";
     }
 }
